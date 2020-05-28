a95ca75177aa5c8febb327e4b4ced1b5bd4881f7
nickBes <bnik.cool@gmail.com>
2020-05-28 22:58:03 +0300

    simple movement and a ground platform

---
+++ trash/Assets/Camera/PlayerFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour{
+    public Transform target;
+    public float smooth = .25f;
+    public Vector3 offset;
+    void Awake() {
+        transform.position = target.position + offset;
+    }
+    void FixedUpdate(){
+        Vector3 progress = Vector3.Lerp(transform.position, target.position + offset, smooth);
+        transform.position = progress;
+    }
+}
---
+++ trash/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    public float[] jump;
+    public float legOffset;
+    public float legLength;
+    public float animSensetivity;
+
+    Animator animator;
+    Rigidbody2D rb;
+    bool onGround;
+    int jumpsLeft;
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void FixedUpdate(){
+        movement();
+        jumping();
+    }
+    void jumping() {
+        onGround = Physics2D.Raycast(transform.position + (Vector3.left * legOffset), Vector2.down, legLength, LayerMask.GetMask("Ground")) ||
+           Physics2D.Raycast(transform.position + (Vector3.right * legOffset), Vector2.down, legLength, LayerMask.GetMask("Ground"));
+        if (onGround) {
+            jumpsLeft = jump.Length;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0) {
+            float jumpStrength = jump[jumpsLeft - 1];
+            rb.velocity = Vector2.up * jumpStrength;
+            jumpsLeft--;
+        }
+    }
+    void movement() {
+        float direction = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+
+        if(direction > 0) {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        } else if(direction < 0){
+            transform.rotation = Quaternion.Euler(Vector3.up * 180);
+        }
+
+        bool moving; 
+        if(Mathf.Abs(direction) >= animSensetivity) {
+            moving = true;
+        } else {
+            moving = false;
+        }
+
+        animator.SetBool("Moving", moving);
+    }
+    void OnDrawGizmos() {
+        Gizmos.color = Color.magenta;
+        //left leg
+        Gizmos.DrawLine(transform.position + (Vector3.left * legOffset), 
+                        transform.position + (Vector3.left * legOffset) + (Vector3.down * legLength));
+        //right leg
+        Gizmos.DrawLine(transform.position + (Vector3.right * legOffset),
+                transform.position + (Vector3.right * legOffset) + (Vector3.down * legLength));
+    }
+}
